5a7ae821dccd42e7c0149d982b3ad599a403423a
Farias <allisson.f.farias@accenture.com>
2021-10-27 18:00:05 -0300

    Atualizacoes para o heroku

--- app.js
+++ app.js
@@ -48,4 +48,4 @@ app.post('/sendMail', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(process.env.PORT, () => console.log('Server is running on port 3000'));
